b64550acd22139ad192b4d0f9df4f5735a7ebe18
jessebrite <jessebrite@gmail.com>
2020-04-02 21:44:26 +0100

    App reworked and works now

--- socketApi.js
+++ socketApi.js
@@ -1,17 +1,27 @@
-var socket_io = require('socket.io');
-var io = socket_io();
-var socketApi = {};
+const socket_io = require('socket.io');
+const io = socket_io();
+const socketApi = {};
 
 socketApi.io = io;
 
 io.on('connection', function(socket) {
+
+  socket.on('user join', function(msg) {
+    this.username = msg;
+    console.log(msg + ' joined')
+    socket.broadcast.emit('user join', msg);
+  });
+
   socket.on('chat message', function(msg) {
-    // io.emit('chat message', msg)
+    msg.username = this.username;
+    // console.log(`${msg.username}: ${msg.message}`)
+    socket.broadcast.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', function(msg) {
+    console.log(this.username + ' left')
+    socket.broadcast.emit('user left', this.username);
   });
 });
-
-socketApi.sendNotification = function() {
-  io.sockets.emit('hello', { msg: 'Hello World!' });
-}
 
 module.exports = socketApi;
